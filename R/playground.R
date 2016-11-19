e9c640b2432d0d20c840a67e3853557b85187ed6
Anton Lebedevich <mabrek@gmail.com>
2016-11-19 22:51:38 +0300

    check naive speedup, more todo

--- R/playground.R
+++ R/playground.R
@@ -1,13 +1,17 @@
+source("make-data.R")
 source("functions.R")
 
 object_size(series)
 
 system.time(cor(series))
 
-options(mc.cores = 1)
-
-system.time(par_cor(series[, 1:100]))
 system.time(cor(series[, 1:100]))
 
-(par_cor(series[, 1:10]))
+options(mc.cores = 1)
+system.time(par_cor_naive(series[, 1:100]))
+
+options(mc.cores = 2)
+system.time(par_cor_naive(series[, 1:100]))
+
+(par_cor_naive(series[, 1:10]))
 (cor(series[, 1:10]))
