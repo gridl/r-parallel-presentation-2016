e55c36f53407b82576496cadcc49f9f66ab2c237
Anton Lebedevich <mabrek@gmail.com>
2016-11-27 12:45:55 +0300

    single foreach parallel cor

--- R/functions.R
+++ R/functions.R
@@ -9,31 +9,6 @@ par_cor <- function(d, block_size = 100) {
   nblocks <- n / block_size
   splits <- split(1:n, rep(1:nblocks, each = block_size))
   combs <- unique(t(apply(expand.grid(1:nblocks, 1:nblocks), 1, sort)))
-  blocks <- mclapply(1:nrow(combs), function(i) {
-    ca <- splits[[combs[i, 1]]]
-    cb <- splits[[combs[i, 2]]]
-    d_a <- d[, ca]
-    d_b <- d[, cb]
-    list(ca, cb, cor(d_a, d_b))
-  })
-  res <- matrix(0, nrow = n, ncol = n)
-  for(block in blocks) {
-    ca <- block[[1]]
-    cb <- block[[2]]
-    res[ca, cb] <- block[[3]]
-    res[cb, ca] <- t(block[[3]])
-  }
-  res
-}
-
-for_cor <- function(d, block_size = 100) {
-  n <- ncol(d)
-  if (n %% block_size != 0) {
-    stop("Choose different 'block_size' so that ncol(x) %% block_size = 0!")
-  }
-  nblocks <- n / block_size
-  splits <- split(1:n, rep(1:nblocks, each = block_size))
-  combs <- unique(t(apply(expand.grid(1:nblocks, 1:nblocks), 1, sort)))
   blocks <- foreach(i = 1:nrow(combs)) %dopar% {
     ca <- splits[[combs[i, 1]]]
     cb <- splits[[combs[i, 2]]]
--- R/playground.R
+++ R/playground.R
@@ -3,28 +3,20 @@ source("functions.R")
 
 object_size(series)
 
+# WAT slowdown slide
 system.time(cor(series))
+
+registerDoParallel(1)
+system.time(par_cor(series))
+
 system.time(cor(series, series))
 
-options(mc.cores = 1)
-system.time(par_cor(series))
-
-options(mc.cores = 2)
-system.time(par_cor(series))
-
-options(mc.cores = 4)
-system.time(par_cor(series))
-
+# WAT different results
 all.equal(cor(series), par_cor(series), tolerance = 0)
 
 all.equal(cor(series), cor(series, series), tolerance = 0)
 
-all.equal(cor(series[, 1:100]), par_cor(series[, 1:100], 50), tolerance = 0)
-
-cor(series[, 1:100])[1,1] - cor(series[,1], series[,1])
 .Machine$double.eps
-
-profvis({ cor(series) })
 
 profvis({ par_cor(series) })
 
@@ -34,14 +26,7 @@ sort(sapply(unique(powerSet(as.integer(factorize(1500)))), prod))
 # TODO grid search to find optimal cores, block size
 
 
-options(mc.cores = 1)
-Rprof(filename = "../output/par_cor.rprof", interval = 0.001, line.profiling = T)
-cr <- par_cor(series, 1500)
-Rprof(NULL)
-
-summaryRprof(filename = "../output/par_cor.rprof", lines = "both")
-
 profvis({ par_cor(series, 1500) })
 
 registerDoParallel(4)
-system.time(for_cor(series))
+system.time(par_cor(series))
