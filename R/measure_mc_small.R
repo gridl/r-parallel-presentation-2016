12263fea52ca3ec82f9d36ef55fc05b831265b1b
Anton Lebedevich <mabrek@gmail.com>
2016-11-30 16:13:41 +0300

    file name

--- R/measure_mc_small.R
+++ R/measure_mc_small.R
@@ -6,4 +6,4 @@ registerDoParallel()
 res <- grid_search(measure_cor, c(1, 2, 4),
                    c(1, 2, 4, 5, 7, 10, 14, 20, 25, 35, 50), 5, setup_mc)
 
-saveRDS(res, "../output/measure_foreach_mc.rds")
+saveRDS(res, "../output/measure_foreach_mc_small.rds")
