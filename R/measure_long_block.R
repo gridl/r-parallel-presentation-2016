e5ec8195877c6d2cc491b54029c5a8bdaf342711
Anton Lebedevich <mabrek@gmail.com>
2016-12-01 10:24:57 +0300

    gather perf cmdlines

--- R/measure_long_block.R
+++ R/measure_long_block.R
@@ -1,8 +1,6 @@
 source("make-data.R")
 source("functions.R")
 
-## perf record -F 999 --call-graph dwarf -o slow.data -a -m 512M -- R CMD BATCH --no-save --no-restore measure_long_block.R
-
 cluster <- makePSOCKcluster(1)
 registerDoParallel(cluster)
 
